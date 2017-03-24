b364bfb6171baefc9310415c5d2c2f06dcc00a2c
Luesa <ludwig_teresa@web.de>
2017-03-24 12:38:08 +0100

    Rename Messgeraet/UserDataType.h to Messgeraet/Programm/UserDataType.h

---
+++ Messgeraet/Programm/UserDataType.h
@@ -0,0 +1,14 @@
+#ifndef UserDataType_h
+#define UserDataType_h
+
+//Anzahl der Sensoren
+const uint8_t ADC_DIM = 3;
+
+struct data_t {
+  //Zeitfeld
+  unsigned short Zeit[4];
+  //Sensorenfeld
+  unsigned short adc[ADC_DIM];
+};
+
+#endif // UserDataType_h
