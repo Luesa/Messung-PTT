8cd3e6d925e1c2c7ac2f43960b8ee1ac403209b3
Luesa <ludwig_teresa@web.de>
2017-03-24 12:47:59 +0100

    Add files via upload

---
+++ GetData/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace SD_Karte_lesen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.PortBox = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.OpenButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OpenButton.Location = new System.Drawing.Point(12, 214);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(104, 38);
+            this.OpenButton.TabIndex = 0;
+            this.OpenButton.Text = "GetData";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // PortBox
+            // 
+            this.PortBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PortBox.FormattingEnabled = true;
+            this.PortBox.Location = new System.Drawing.Point(12, 150);
+            this.PortBox.Name = "PortBox";
+            this.PortBox.Size = new System.Drawing.Size(121, 28);
+            this.PortBox.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(248, 120);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Bitte den COM Port des angeschlossenen Gerätes auswählen. Die zugehörige Nummer\r\n" +
+    "kann unter Windows im Gerätemanager eingesehen\r\nwerden.";
+            // 
+            // Output
+            // 
+            this.Output.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Output.Location = new System.Drawing.Point(286, 13);
+            this.Output.Name = "Output";
+            this.Output.ReadOnly = true;
+            this.Output.Size = new System.Drawing.Size(590, 369);
+            this.Output.TabIndex = 4;
+            this.Output.Text = "";
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseButton.Location = new System.Drawing.Point(12, 329);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(104, 38);
+            this.CloseButton.TabIndex = 5;
+            this.CloseButton.Text = "Schließen";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(122, 214);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(125, 52);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Lädt die Dateien von der SD Karte auf den Computer";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.White;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(122, 329);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(125, 38);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "Schließt das Programm";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(912, 394);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.PortBox);
+            this.Controls.Add(this.OpenButton);
+            this.Name = "Form1";
+            this.Text = "GetData";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.ComboBox PortBox;
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.Button CloseButton;
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ GetData/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+
+namespace SD_Karte_lesen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //Komponente aktivieren
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Lädt das Fenster/die GUI, sowie die verfügbaren Ports und lädt die Grundeinstellung für die Tasten
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();         //Alle verfügbaren Ports in einem string speichern
+            textBox1.Enabled = false;                           //Anweisungsbox ist nicht aktiv
+            PortBox.Items.AddRange(ports);                      //Portnames werden in die Liste eingefügt
+        }
+
+        /// <summary>
+        /// Sobald der Benutzer auf Verbinden klickt, werden die Dateien von dem Gerät gelesen und in einer Datei gespeichert
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            Output.Clear();                                                         //Ausgabefeld leeren
+            if (PortBox.Text == "")                                                 //Sollte nichts ausgewählt sein,
+            {                                                                       //gibt es eine Fehlermeldung
+                Output.Text = "Bitte COM Port auswählen!\n";
+            }
+            else
+            {
+                OpenButton.Enabled = false;                                         //Verbindungs Feld deaktivieren
+                PortBox.Enabled = false;                                            //Auswahlfeld deaktivieren
+                
+                serialPort1.PortName = PortBox.Text;                                //Name des Seriellen Ports festlegen
+                serialPort1.ReadTimeout = 3000;                                     //Timeout für Lesen. Wenn nach 5s der Terminator nicht erreicht wurde, wird ein Fehler ausgegeben         
+                serialPort1.BaudRate = 1500000;                                      //Baudrate Einstellungen. Für Teensy unerheblich, da dieser immer mit maximaler USB Geschwindigkeit überträgt
+                serialPort1.Open();                                                 //Öffnen des COM Ports
+                Output.Text = PortBox.Text + " wurde geöffnet.\n\n";                //Benutzerausgabe
+                serialPort1.WriteLine("a\n");                                       //Befehl an Gerät senden
+                
+                //Anzahl der Binärdateien ermitteln
+                Variables.Anzahl = short.Parse(serialPort1.ReadLine());             //Anzahl der Binärdateien einlesen
+                int i = 0;                                                          //Zählindex für Schleifen
+                serialPort1.ReadLine();                                             //Bei Beginn einer Datei wird 'neu' gesendet. Bei der ersten Datei wird dies nicht benötigt
+                                                                                                    
+                for(i=0;i<8;i++)                                                    //die Zeichen für das Datum werden einzeln eingelesen, da Arduino sonst Fehler einbaut
+                {
+                    Variables.date[i] = (char)serialPort1.ReadChar();               //Die Zeichen werden als Char eingelesen
+                    Variables.datum = Variables.datum + Variables.date[i].ToString();   //und zu einem String zusammengefügt
+                }
+                //serialPort1.ReadLine();                                             //Zusätzliches \r einlesen
+                //serialPort1.ReadLine();
+                int filenumber = 1;                                                 //aktuelle Dateinummer festlegen
+                string Zeile = "";                                                  //String für das Einlesen der Zeilen anlegen
+                //serialPort1.ReadLine();
+                //Ab hier werden die gespeicherten Dateien ausgelesen
+                while (Variables.Anzahl > 0)
+                {
+                    do {
+                        Variables.filename = Variables.datum + "_" + filenumber + ".csv";   //Dateiname erstellen, im Format Jahr_Monat_Tag_Nummer
+                        Variables.filename = Variables.filename.Replace("\r", "");          //\r löschen
+
+                        Variables.foldername = @"c:\PTT-Messung\";                          //erstellen eines Pfades für das Anlegen einer Datei.
+                        System.IO.Directory.CreateDirectory(Variables.foldername);          //Der Pfad wird angelegt
+                        Variables.path = System.IO.Path.Combine(Variables.foldername, Variables.filename);//Die Datei wird zum Pfad hinzugefügt
+                        filenumber++;                                                       //Erhöhen der Dateinummer
+                    }while(System.IO.File.Exists(Variables.path));                          //Solange die Dateinummer erhöhen, bis ein Dateiname gefunden wurde, der noch nicht verwendet wurde
+                                                                                            //Bsp. gleiches Datum, unterschiedlicher Übertragungsdurchlauf
+
+                    //Instanz aktivieren, die das Schreiben in eine Datei möglich macht
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(Variables.path, true))                         //Pfad und Datei werden erstellt
+                    {
+                        Output.AppendText("Schreibe in Datei "); Output.AppendText(Variables.filename + "\r");  //Ausgabe
+
+                        while (true)    //Endlosschleife
+                        {
+                            try
+                            {
+                                Zeile = serialPort1.ReadLine();     //aktuelle Zeile auslesen
+                            }
+                            catch                                   //Abfang Protokoll, sollte eine Ausnahme entstehen, z.B. konnte nichts eingelesen werden
+                            {
+                                Zeile = "";                         //Für zusätzliche Abfragen, ob das Auslesen gklappt hat
+                                break;                              //alle Dateien wurden ausgelesen und das Programm kann beendet werden
+                            }
+
+                            bool Patient = Zeile.Contains("neu");      //Frage, ob wir auf eine Zeile mit neu gestoßen sind, dies bedeutet, das die Datei zu einem neuen Patienten gehört
+                            if (Patient == true)                       //Wenn ja, beginnt hier eine neue Teildatei
+                            {
+                                Variables.Anzahl--;                    //Anzahl der verfügbaren Dateien um 1 verkleinern
+                                
+                                //Datum auslesen
+                                for (i=0;i<8;i++)
+                                {
+                                    Variables.date[i] = (char)serialPort1.ReadChar();
+                                    Variables.datum2 = Variables.datum2 + Variables.date[i].ToString();
+                                }
+                                serialPort1.ReadLine(); // \r auslesen
+
+                                if (Variables.datum == Variables.datum2)       //Die Datei wurde an demselben Datum aufgenommen, wie die vorige
+                                {
+                                    Variables.datum2 = "";
+                                    break;
+                                }
+                                else
+                                {
+                                    Variables.datum = Variables.datum2;
+                                    Variables.datum2 = "";
+                                    filenumber=1;
+                                    break;
+                                }//Ende if(Zeile == Datum)
+                            }//Ende if(Datum)
+
+                            file.WriteLine(Zeile);      //ausgelesene Zeile in Datei schreiben. Achtung: Sollte die Datei schon Inhalte enthalten, werden die neuen einfach angehängt!!!
+
+                        }//Ende Schleife zum Schreiben in Datei
+
+                        //Abbruchbedingung
+                        if(Zeile=="")
+                        {
+                            break;
+                        }
+                        
+                    }
+                }
+                //Benutzerausgabe
+                Output.AppendText("\nFertig\n");
+                Output.AppendText("Sie finden Ihre Dateien im Verzeichnis:\n");
+                Output.AppendText(Variables.foldername + "\n");
+                serialPort1.Close();
+                PortBox.Enabled = true;
+            }
+        }
+        
+        
+        /// <summary>
+        /// Schließt die Anwendung
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            //Beenden des Programms
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+                       
+                Application.Exit();
+            
+        }
+    }
+}
